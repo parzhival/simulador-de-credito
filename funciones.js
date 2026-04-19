03eff964ed4196967635c56a23abad77de4723d4
parzhival <jean08070225@gmail.com>
2026-04-19 13:42:56 -0500

    feat: Features and style changes are added

--- funciones.js
+++ funciones.js
@@ -1,1 +1,39 @@
-//AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+//AQUI TODA LA LOGICA DE LAS FUNCIONES DEL PROYECTO
+function calcularDisponible(ingresos,egresos){
+    let disponible;
+    disponible=ingresos-egresos;
+    if (disponible<0){
+    disponible=0
+    }    
+    return disponible;
+}
+function calcularCapacidadPago(montoDisponible){
+    let monto;
+    monto=montoDisponible/2;
+    return monto;
+}
+function calcularInteresSimple(monto,tasa,tiempo){
+    let cuota;
+    cuota=tiempo*monto*(tasa/100);
+    return cuota;
+}
+function calcularTotalPagar(monto,tasa){
+    let totalPagar;
+    totalPagar=monto+tasa+100;
+    return totalPagar;
+}
+function calcularCuotaMensual(total,tiempo){
+    let cuotaMensual;
+    cuotaMensual=total/(tiempo*12);
+    return cuotaMensual;
+}
+function aprobarCredito(capacidadPago,cuotaMensual){
+    if(capacidadPago >  cuotaMensual){
+        
+        return true
+    }else{
+        
+        return false
+    }
+
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,68 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular(){
+    let ingresos=document.getElementById("txtIngresos");
+    let egresos=document.getElementById("txtEgresos");
+    let valorDisponible1;
+    let valorDisponible;
+    let ingresosFloat;
+    let egresosFloat;
+    let total;
+    let capacidadPago1;
+    let capacidadPago;
+    let pago;
+    let monto=document.getElementById("txtMonto");
+    let tiempo=document.getElementById("txtPlazo");
+    let tasa=document.getElementById("txtTasaInteres");
+    let montoInt;
+    let tiempoInt;
+    let tasaInt;
+    let cuota;
+    let cuota1;
+    let cuota2;
+    let totalPagar;
+    let totalPagar1;
+    let cuotaMensual;
+    let cuotaMensual1;
+    let cuotaMensualFinal;
+    let credito1;
+    let credito;
+
+    ingresosFloat=parseFloat(ingresos.value);
+    egresosFloat=parseFloat(egresos.value);
+
+    valorDisponible1=calcularDisponible(ingresosFloat,egresosFloat);
+    valorDisponible=valorDisponible1.toFixed(2);
+    total=document.getElementById("spnDisponible");
+    total.textContent=valorDisponible;
+
+    capacidadPago1=calcularCapacidadPago(valorDisponible);
+    capacidadPago=capacidadPago1.toFixed(2);
+    pago=document.getElementById("spnCapacidadPago");
+    pago.textContent=capacidadPago;
+
+    montoInt=parseInt(monto.value);
+    tiempoInt=parseInt(tiempo.value);
+    tasaInt=parseInt(tasa.value);
+
+    cuota=calcularInteresSimple(montoInt,tasaInt,tiempoInt);
+    cuota1=cuota.toFixed(2);
+    cuota2=document.getElementById("spnInteresPagar");
+    cuota2.textContent=cuota1;
+
+    totalPagar=calcularTotalPagar(montoInt,cuota);
+    totalPagar1=document.getElementById("spnTotalPrestamo");
+    totalPagar1.textContent=totalPagar;
+
+    cuotaMensual=calcularCuotaMensual(totalPagar,tiempoInt);
+    cuotaMensual1=cuotaMensual.toFixed(2);
+    cuotaMensualFinal=document.getElementById("spnCuotaMensual");
+    cuotaMensualFinal.textContent=cuotaMensual1;
+
+    credito1=aprobarCredito(capacidadPago1,cuotaMensual);
+    credito=document.getElementById("spnEstadoCredito");
+    if(credito1==true){
+        credito.textContent="CREDITO APROBADO";
+    }else{
+        credito.textContent="CREDITO RECHAZADO";
+    }
+}
